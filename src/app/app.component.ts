2e57b8b02e51fc273c8e3ad17a154688289ce65a
Nguyen Truong Hoang <truonghoangnguyen@gmail.com>
2017-09-02 21:30:19 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,26 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'app';
+// }
+
+
+import { Component, Input, OnChanges } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+export class InvoiceItem {
+  stt = '';
+  name = '';
+  unit = '';
+  qty = '';
+  cost = '';
+  total = 0;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +28,86 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  form: FormGroup;
+  customer_info = {
+    name: 'Mr. John Doe',
+    web_link: 'John Doe Designs Inc.',
+    address1: '1 Infinite Loop',
+    address2: 'Cupertino, California, US',
+    postal: '90210'
+  };
+  company_info = {
+    name: 'Metaware Labs',
+    web_link: 'www.metawarelabs.com',
+    address1: '123 Yonge Street',
+    address2: 'Toronto, ON, Canada',
+    postal: 'M5S 1B6'
+  };
+
+  public invoice = {items: [],
+    customer_info: this.customer_info,
+    company_info: this.company_info
+  };
+
+  logoSrc = 'http://metaware.github.io/angular-invoicing/images/metaware_logo.png';
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+    this.addItem();
+  }
+
+  get invoiceItems(): FormArray {
+    return this.form.get('invoiceItems') as FormArray;
+  };
+
+  toggleLogo() {}
+  editLogo() {}
+
+  addItem() {
+    this.invoiceItems.push(this.fb.group(new InvoiceItem()));
+  }
+
+  removeItem(item) {
+
+    let i = this.invoiceItems.controls.indexOf(item);
+
+    if(i != -1) {
+    	this.invoiceItems.controls.splice(i, 1);
+      let items = this.form.get('invoiceItems') as FormArray;
+    	let data = {invoiceItems: items};
+    	this.updateForm(data);
+    }
+  }
+
+  onSubmit(){//f: NgForm){
+
+  }
+
+  updateForm(data) {
+    const items = data.invoiceItems;
+    let sub = 0;
+    for(let i of items){
+      i.total = i.qty*i.cost;
+      sub += i.total;
+    }
+    this.form.value.subTotal = sub;
+    const tax = sub * (this.form.value.taxPercent / 100);
+    this.form.value.tax = tax;
+    this.form.value.grantTotal = sub + tax;
+  }
+  createForm() {
+    this.form = this.fb.group({
+      invoiceName: ['', Validators.required ],
+      invoiceItems: this.fb.array([]),
+      subTotal:[{value: 0, disabled: true}],
+      taxPercent:[],
+      tax: [0],
+
+      grantTotal:[{value: 0, disabled: true}],
+    });
+
+   this.form.valueChanges.subscribe(data => this.updateForm(data));
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,99 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
+import 'hammerjs';
+import {CdkTableModule} from '@angular/cdk/table';
+import { CurrencyPipe } from '@angular/common';
+import {
+  MdAutocompleteModule,
+  MdButtonModule,
+  MdButtonToggleModule,
+  MdCardModule,
+  MdCheckboxModule,
+  MdChipsModule,
+  MdCoreModule,
+  MdDatepickerModule,
+  MdDialogModule,
+  MdExpansionModule,
+  MdGridListModule,
+  MdIconModule,
+  MdInputModule,
+  MdListModule,
+  MdMenuModule,
+  MdNativeDateModule,
+  MdPaginatorModule,
+  MdProgressBarModule,
+  MdProgressSpinnerModule,
+  MdRadioModule,
+  MdRippleModule,
+  MdSelectModule,
+  MdSidenavModule,
+  MdSliderModule,
+  MdSlideToggleModule,
+  MdSnackBarModule,
+  MdSortModule,
+  MdTableModule,
+  MdTabsModule,
+  MdToolbarModule,
+  MdTooltipModule,
+} from '@angular/material';
+
+
+@NgModule({
+  exports: [
+    CdkTableModule,
+    MdAutocompleteModule,
+    MdButtonModule,
+    MdButtonToggleModule,
+    MdCardModule,
+    MdCheckboxModule,
+    MdChipsModule,
+    MdCoreModule,
+    MdDatepickerModule,
+    MdDialogModule,
+    MdExpansionModule,
+    MdGridListModule,
+    MdIconModule,
+    MdInputModule,
+    MdListModule,
+    MdMenuModule,
+    MdNativeDateModule,
+    MdPaginatorModule,
+    MdProgressBarModule,
+    MdProgressSpinnerModule,
+    MdRadioModule,
+    MdRippleModule,
+    MdSelectModule,
+    MdSidenavModule,
+    MdSliderModule,
+    MdSlideToggleModule,
+    MdSnackBarModule,
+    MdSortModule,
+    MdTableModule,
+    MdTabsModule,
+    MdToolbarModule,
+    MdTooltipModule,
+  ]
+})
+export class MyMaterialModule {}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    // HttpModule,
+    MyMaterialModule,
+    MdNativeDateModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
